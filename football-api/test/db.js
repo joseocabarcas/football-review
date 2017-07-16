7bc2a61152d79b48de19568cb6c00de1c18f00d4
José Cabarcas <josecabarcas94@gmail.com>
2017-07-16 12:56:48 -0500

    added bcrypt dependency

--- football-api/test/db.js
+++ football-api/test/db.js
@@ -13,6 +13,18 @@ test.beforeEach('connect db', async t => {
 })
 
 test('save user', async t => {
-	console.log(t.context.db)
+	let db = t.context.db
+	t.is(typeof db.saveUser, 'function', 'saveUser is a function')
+
+	let user = fixtures.getUser()
+	let passPlain = user.password
+	let created = await db.saveUser(user)
+
+	t.is(user.username, created.username)
+	t.is(user.email, created.email)
+	t.is(user.name, created.name)
+	t.is(utils.encrypt(plainPassword), created.password)
+	t.is(typeof user.id, 'string')
+	t.truthy(created.createdAt)
 	t.true(true)
 })
