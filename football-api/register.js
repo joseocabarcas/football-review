cbd217106fab1ea0bb726475cabbae038ff8136d
Jose Cabarcas <jcabarcas@joonik.com>
2017-07-19 17:04:50 -0500

    working upload

--- football-api/register.js
+++ football-api/register.js
@@ -1,21 +1,24 @@
 const { send, json } = require('micro');
 const HttpHash = require('http-hash');
+const DB = require('./db');
 
 const hash = HttpHash()
+const db = new DB()
 
 hash.set('POST /register', async function register (req, res, params) {
 	const data = {'data': 'message'}
 	const statusCode = 200
 
     try{
-        var da = await json(req)
+        var user = await json(req)
+        console.log(user)
+        await db.connect()
+        var response = await db.saveUser(user)
+        console.log(response)
     } catch(e) {
         console.log(e)
         throw e;
     }
-
-	console.log(da)
-    debugger;
 
 	send(res, statusCode, data)
 })
