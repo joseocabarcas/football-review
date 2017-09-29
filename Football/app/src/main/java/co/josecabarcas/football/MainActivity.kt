b6a71350e2aa9e9242fc2e67c48af288314b39b6
Jose Cabarcas <jcabarcas@joonik.com>
2017-09-29 16:58:35 -0500

    add clicklistener

--- Football/app/src/main/java/co/josecabarcas/football/MainActivity.kt
+++ Football/app/src/main/java/co/josecabarcas/football/MainActivity.kt
@@ -3,12 +3,15 @@ package co.josecabarcas.football
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hello.text = "Hola mundo"
+        login.setOnClickListener({ v -> run {
+            toast("Hello")
+        } })
     }
 }
